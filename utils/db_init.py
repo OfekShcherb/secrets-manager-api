b566d2fc128cd7a0a6fd202c9326ef2ab458c250
Ofek Shcherb <ofek.shcherb98@gmail.com>
2025-06-26 13:23:25 +0300

    Refactor code

--- utils/db_init.py
+++ utils/db_init.py
@@ -1,13 +1,14 @@
-import sqlite3
+from sqlite3 import connect
 import os
 from utils.db import rotate_old_keys
 
 def initialize_db():
     if not os.path.exists('secrets.db'):
-        with sqlite3.connect('secrets.db') as conn:
+        with connect('secrets.db') as conn:
             c = conn.cursor()
             c.execute('''CREATE TABLE users (
                       username TEXT PRIMARY KEY,
+
                       password TEXT
                       )'''
                     )
